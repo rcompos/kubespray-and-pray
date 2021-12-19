6fa28fd28bc4105cd768fb5c97ce2ed7422f4e56
Ron Compos <rcompos@gmail.com>
2021-12-18 19:32:03 -0700

    Updates for Kubernets v1.21.6.

---
+++ New/argocd.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# Install ArgoCD
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
---
+++ inventory/dev-lucky/loadbalance/README.md
@@ -0,0 +1,3 @@
+# Install MetalLB Helm chart
+
+helm -n load-balance install metallb metallb/metallb -f values.yaml
